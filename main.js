aeb0d4f103498f8e92fc5b7a4845937c7418a509
Kelwin Hollunder <kelwin.hollunder@fengbrasil.com.br>
2023-05-27 13:06:52 -0300

    new crud

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 const produtos = [
     {
-        nome: 'Bola',
-        valor: 25,
+        nome: 'Teclado',
+        valor: 250,
         estoque: 10
     },
     {
-        nome: 'Tênis',
-        valor: 50,
+        nome: 'Mouse',
+        valor: 150,
         estoque: 20
     }
 ];
 const clientes = [
     {
         nome: 'Kelwin',
-        saldo: 150,
+        saldo: 1500,
         produtos: []
     },
     {
         nome: 'Karla',
-        saldo: 40,
+        saldo: 400,
         produtos: []
     }
-]
+];
 
 //Criar produto (create)
 function criaProduto(nome, valor, estoque) {
-    const obj = {nome: nome, valor: valor, estoque: estoque}
-    produtos.push(obj)
+    const obj = {nome: nome, valor: valor, estoque: estoque};
+    produtos.push(obj);
 }
 
 //Ver Produtos (read)
@@ -37,51 +37,47 @@ function verProdutos() {
 
 //Atualizar produtos(update)
 function atualizaProduto(nome, novoValor, novoEstoque) {
-    const index = produtos.findIndex(produto => produto.nome === nome);
+    const index = produtos.findIndex(item => item.nome == nome);
+
     if(novoValor) {
         produtos[index].valor = novoValor;
     }
     if(novoEstoque) {
-        produtos[index].estoque = novoEstoque; 
+        produtos[index].estoque = novoEstoque;
     }
 }
 
 //Excluir produtos(delete)
 function excluirProduto(nome) {
-    const index = produtos.findIndex(produto => produto.nome === nome);
-    produtos.splice(index, 1)
+    const index = produtos.findIndex(item => item.nome == nome);
+    produtos.splice(index, 1);
 }
 
-
-
-
 //COMPRAR PRODUTOS
-function comprarProdutos(produto, cliente, quantidade) {
-    const produtoIndex = produtos.findIndex(item => item.nome === produto);
-    const clienteIndex = clientes.findIndex(item => item.nome === cliente);
-
+function comprarProduto(nomeProduto, nomeCliente, quantidade) {
+    const produtoIndex = produtos.findIndex(item => item.nome == nomeProduto);
+    const clienteIndex = clientes.findIndex(item => item.nome == nomeCliente);
     const valorDaCompra = produtos[produtoIndex].valor * quantidade;
 
     if(clientes[clienteIndex].saldo >= valorDaCompra) {
-        console.log(`Compra aprovada`)
         clientes[clienteIndex].saldo = clientes[clienteIndex].saldo - valorDaCompra;
         produtos[produtoIndex].estoque = produtos[produtoIndex].estoque - quantidade;
         let i = 1;
         while(i <= quantidade) {
             i++;
-            clientes[clienteIndex].produtos.push(produto);
+            clientes[clienteIndex].produtos.push(nomeProduto);
         }
         console.log(clientes[clienteIndex]);
         console.log(produtos[produtoIndex]);
     } else {
-        console.log('Saldo insuficiente.')
+        console.log('Saldo insuficiente');
     }
 
 }
 
-comprarProdutos('Bola', 'Kelwin', 4)
-// criaProduto('Luvas', 20, 150);
-// atualizaProduto('Bola', null, 40)
-// excluirProduto('Tênis');
-
+// criaProduto('Monitor', 500, 1000);
+// atualizaProduto('Teclado', 1000, 20)
+// excluirProduto('Mouse');
+comprarProduto('Teclado', 'Kelwin', 3);
 // verProdutos();
+
